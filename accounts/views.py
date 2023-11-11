c62b9bbd3422aba4604e26821e79628ab588ed7a
JiSeong Lee <52674782+easycastle@users.noreply.github.com>
2023-11-11 18:28:33 +0900

    Jwt (#2)
    * Feat: Add RegisterAPIView
    
    * Feat: Add AuthAPIView
    
    * Fix: email -> username 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,41 +1,101 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib import auth
-from django.views.generic import CreateView
-from accounts.forms import SignupForm
+import jwt
+from rest_framework.views import APIView
+from .serializers import *
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from django.shortcuts import render, get_object_or_404
+from donguramii.settings import SECRET_KEY
 
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            
+            # jwt 토큰 접근
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "register successs",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            
+            # jwt 토큰 => 쿠키에 저장
+            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("refresh", refresh_token, httponly=True)
+            
+            return res
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class AuthAPIView(APIView):
+    def get(self, request):
+        try:
+            access = request.COOKIES['access']
+            payload = jwt.decode(access, SECRET_KEY, algorithms=['HS256'])
+            pk = payload.get('user_id')
+            user = get_object_or_404(User, pk=pk)
+            serializer = UserSerializer(instance=user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
-class SignupView(CreateView):
-    form_class = SignupForm
-    template_name = 'signup.html'
-    success_url = 'login/'
+        except(jwt.exceptions.ExpiredSignatureError):
+            data = {'refresh': request.COOKIES.get('refresh', None)}
+            serializer = TokenRefreshSerializer(data=data)
+            if serializer.is_valid(raise_exception=True):
+                access = serializer.data.get('access', None)
+                refresh = serializer.data.get('refresh', None)
+                payload = jwt.decode(access, SECRET_KEY, algorithms=['HS256'])
+                pk = payload.get('user_id')
+                user = get_object_or_404(User, pk=pk)
+                serializer = UserSerializer(instance=user)
+                res = Response(serializer.data, status=status.HTTP_200_OK)
+                res.set_cookie('access', access)
+                res.set_cookie('refresh', refresh)
+                return res
+            raise jwt.exceptions.InvalidTokenError
 
-def signup(request):
-    if request.method == 'POST':
-        if request.POST['password'] == request.POST['confirm']:
-            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
-            auth.login(request, user)
-            return redirect('/')
-    return render(request, 'signup.html')
+        except(jwt.exceptions.InvalidTokenError):
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
-def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = auth.authenticate(request, username=username, password=password)
-        
+    def post(self, request):
+        user = authenticate(
+            username=request.data.get("username"), password=request.data.get("password")
+        )
         if user is not None:
-            auth.login(request, user)
-            return redirect('/')
+            serializer = UserSerializer(user)
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "login success",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("refresh", refresh_token, httponly=True)
+            return res
         else:
-            return render(request, 'login.html', {'error' : 'username or password is incorrect.'})
-    else:
-        return render(request, 'login.html')
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
-def logout(request):
-    if request.method == 'POST':
-        auth.logout(request)
-        return redirect('/')
-
-    return render(request, 'login.html')
+    def delete(self, request):
+        response = Response({
+            "message": "Logout success"
+            }, status=status.HTTP_202_ACCEPTED)
+        response.delete_cookie("access")
+        response.delete_cookie("refresh")
+        return response
