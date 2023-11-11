f3443bc9f7c531ee4cad80c67a47904f3a99b149
cocoquiet <cocoquiet@knu.ac.kr>
2023-11-11 13:27:08 +0900

    Feat: login, signup, logout 추가

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
     profile_image = models.ImageField(blank=True, upload_to='profile_image')
