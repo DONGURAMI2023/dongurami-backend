4da8bbeaf7cbcaa1363ee77ebc3324bbee8cdda0
JongunJeong <skeep194@knu.ac.kr>
2023-11-12 01:47:28 +0900

    add buy area

--- area/urls.py
+++ area/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = "area"
 urlpatterns = [
     path("", views.get_area, name="area"),
-    path("<int:area_id>/take/<int:user_id>", views.take_area, name='take_area'),
+    path("<int:area_id>/take/<int:user_id>", views.BuyAreaAPIView.as_view()),
     path("insert_demo", views.demo, name="insert_demo")
 ]
